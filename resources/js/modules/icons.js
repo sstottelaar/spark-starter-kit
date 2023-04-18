4ef761e20a909498f7b8e1c912800fc0b964ca83
S. Stottelaar <stottelaar.s@gmail.com>
2023-04-18 21:43:57 +0200

    Updated files

---
+++ resources/js/modules/icons.js
@@ -0,0 +1,28 @@
+import {
+    createIcons,
+    Users,
+    Settings2,
+    BarChart3,
+    ArrowUpDown,
+    MonitorSmartphone,
+    FilePieChart,
+    User,
+    CheckCircle,
+    Plus,
+    Minus,
+} from "lucide";
+
+createIcons({
+    icons: {
+        Users,
+        Settings2,
+        BarChart3,
+        ArrowUpDown,
+        MonitorSmartphone,
+        FilePieChart,
+        User,
+        CheckCircle,
+        Plus,
+        Minus,
+    },
+});
---
+++ resources/js/site.js
@@ -0,0 +1,12 @@
+import "./modules/icons";
+import "./modules/alpine";
+import "@fontsource/inter";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/plus-jakarta-sans";
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/600.css";
+import "@fontsource/plus-jakarta-sans/700.css";
